c5ac4fc7bdbc034efadc6ae16aeb2f26689b7ff9
cbachl <bachl.christoph@gmx.at>
2022-05-27 15:46:26 +0200

    add helper screen text

--- Android_App/app/src/main/java/com/example/youngcarers/screens/Help_Screen.kt
+++ Android_App/app/src/main/java/com/example/youngcarers/screens/Help_Screen.kt
@@ -3,31 +3,44 @@ package com.example.youngcarers
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun Help_Screen() {
     Column(
         modifier = Modifier
+            //.size(100.dp)
             .fillMaxSize()
             .background(Color.LightGray)
-            .wrapContentSize(Alignment.Center)
+            .verticalScroll(rememberScrollState())
+            .padding(bottom = 80.dp)
+
     ) {
-        Text("Help View",
+
+        Text(
+            "Hallo!",
+            color = colorResource(id = R.color.yc_red_dark),
+            fontSize = 35.sp,
             fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp
+            modifier = Modifier.padding(start = 20.dp, top = 60.dp)
+        )
+        Text(
+            "Hier bist du auf der Young Carers App gelandet. Vielleicht hast du ja eine der folgenden Fragen oder du erlebst auch gerade eine dieser Situationen?",
+            modifier = Modifier.padding(start = 20.dp, top = 10.dp, end = 20.dp)
         )
     }
 }
