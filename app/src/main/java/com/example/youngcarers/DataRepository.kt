88ac243747b086d8d6918c6c60025d3e0928126d
Gerald Diesenreiter <gerald.diesenreiter@gmx.at>
2022-07-22 14:54:15 +0200

    add Injection Modules and Content Repository

---
+++ app/src/main/java/com/example/youngcarers/DataRepository.kt
@@ -0,0 +1,19 @@
+package com.example.youngcarers
+
+import com.example.youngcarers.data.api.models.YoungCarersModel
+import com.example.youngcarers.data.api.service.APIService
+
+interface DataRepository {
+    suspend fun loadContent() : YoungCarersModel?
+}
+
+class DataRepositoryImpl : DataRepository {
+
+    override suspend fun loadContent(): YoungCarersModel? {
+        val apiService = APIService.getInstance()
+        return apiService
+                .getContent()
+                .body()
+    }
+
+}
